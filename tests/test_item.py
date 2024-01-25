4955012271c930534d799f18c675641f2a2a31c6
Olga Aukhadieva <sarole4ka@gmail.com>
2024-01-25 20:32:07 +0500

    Added tests for class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,12 +2,36 @@
 import pytest
 
 from src.item import Item
+from config import ROOT
+import os
 
+CSV_PATH = os.path.join(ROOT, 'src', 'items.csv')
 
 @pytest.fixture
 def item_for_test():
     return Item("Смартфон", 10000, 20)
 
+# @pytest.fixture
+# def csv_for_test():
+#     return
+
+def test_name(item_for_test):
+    assert item_for_test.name == 'Смартфон'
+
+def test_name_setter(item_for_test):
+    item_for_test.name = 'СуперСмартфон'
+    assert item_for_test.name == 'СуперСмарт'
+
+def test_instantiate_from_csv(item_for_test):
+    item_for_test.instantiate_from_csv(CSV_PATH)  # создание объектов из данных файла
+    assert len(Item.all) == 6
+    item = item_for_test.all[1]
+    assert item.name == 'Смартфон'
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
 
 def test_calculate_total_price(item_for_test):
     assert item_for_test.calculate_total_price() == 200000
