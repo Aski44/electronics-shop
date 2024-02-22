d25dbc4649b3132d43ed4627fc5c8968e13a484f
Sasha <poriadchenko@yandex.ru>
2024-02-23 02:35:46 +0300

    1

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,1 +1,35 @@
-1
+from src.item import Item
+
+
+class MixinLang:
+    """Класс-миксин для товара 'клавиатура'."""
+
+    LANG = ['EN', 'RU']
+
+    def __init__(self):
+        self.__language = self.LANG[0]
+
+    @property
+    def language(self):
+        """Геттер для чтения приватного 'language'."""
+        return self.__language
+
+    def change_lang(self):
+        """Метод для изменения языка (раскладки клавиатуры)."""
+        if self.language == self.LANG[0]:
+            self.__language = self.LANG[1]
+        else:
+            self.__language = self.LANG[0]
+        return self.language
+
+
+class Keyboard(Item, MixinLang):
+    """Класс для товара 'клавиатура'."""
+
+    def __init__(self, name: str, price: float, quantity: int):
+        """
+        Расширяет функционал класса 'Item'.
+        Добавляет дополнительный атрибут 'language'.
+        """
+        super().__init__(name, price, quantity)
+        self.__language = "EN"
